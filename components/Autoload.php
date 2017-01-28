94e86ab74241b4ac91fbd06d5cae0c9c003fe76b
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2017-01-28 22:36:37 +0200

    removed file_exists() checking in autoload

--- components/Autoload.php
+++ components/Autoload.php
@@ -10,8 +10,6 @@ spl_autoload_register(function ($className) {
     
     foreach ($arrPaths as $path) {
         $path = ROOT . $path . $className . '.php';
-        if (file_exists($path)) {
-            include_once($path);
-        }
+        require_once($path);        
     }
 });
