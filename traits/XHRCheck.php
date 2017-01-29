e7c96674accefacefc0ddd15abe02a6c5375994e
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2017-01-29 23:08:52 +0200

    deleted unnecessary files

--- traits/XHRCheck.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-trait XHRCheck {
-    public function isXHR() {
-        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-            $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
-    }
-}
